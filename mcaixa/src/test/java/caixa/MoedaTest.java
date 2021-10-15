3971ccdee0d25c0aa749239ad5f5e5be44883ca6
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2021-10-15 20:43:45 -0300

    Exemplo de dublê em Java usando Mockito via Maven #8

--- mcaixa/src/test/java/caixa/MoedaTest.java
+++ mcaixa/src/test/java/caixa/MoedaTest.java
@@ -3,6 +3,7 @@ package caixa;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 class MoedaTest {
 
@@ -12,4 +13,13 @@ class MoedaTest {
 		Moeda moeda = new Moeda(1,"BRL");
 		assertEquals((float)1/5,moeda.converterPara("USD"));
 	}
+	
+	@Test
+	public void converterReaisDubleEmDolar()
+	{
+		Moeda moeda = mock(Moeda.class);
+		when(moeda.converterPara("USD")).thenReturn((float)0.2);
+		assertEquals((float)1/5,moeda.converterPara("USD"));
+	}
+	
 }
